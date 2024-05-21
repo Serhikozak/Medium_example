13fcc8235b949aaa884fa90f755611833050ce89
Serhikozak <skozakov924@gmail.com>
2024-05-21 21:26:17 +0300

    The end

--- counter-service.py
+++ counter-service.py
@@ -19,4 +19,59 @@ def read_counter():
     else:
         return 0
 
-print("Hello")        
+def update_counter(counter):
+    """
+    Updates the counter file with the new counter value.
+    If the file doesn'n exist, it return 0.
+
+    Arg:
+        counter(int): The new counter value to write to the file.
+    """
+    with open(COUNTER_FILE, "w") as f:
+        f.write(str(counter))
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+    """
+    Handles GET  and POST requests to the root endpoint.
+    GET request returns the current count of POST requests.
+    POST requests increments the counter and returns the updated count.
+
+    Returns:
+        str: The response message with the current or update counter.
+    """
+    counter = read_counter()
+    if request.method == 'POST':
+        # Increment the counter for each POST request and update the file.
+        counter += 1
+        update_counter(counter)
+        return f"POST request counter updated. Current count: {counter} "
+    else:
+        # For GET requests, just return the current count.
+        return f"Current POST requests count: {counter}"
+@app.route('/health', methods=['GET'])
+
+def health_check():
+    """
+    Performs a simple health check of the application.
+    It tries to read the counter file as a basic check.
+    Returns:
+        turple: A JSON response indicating the health status and the HTTP statuscode.
+    """
+    try:
+        # Basic health check: Ensure the counter file is accessible.
+        read_counter()
+        return jsonify({"status": "healthy"}), 200
+    except Exception as e:
+        # Return an unheathy status if any error occurs, e.g. file acces issues.
+        return jsonify({"status": "unhealthy", "reason": str(e)}), 500
+if __name__=='__name__':
+    # Run the Flask app with binding to all interfaces on port 8080.
+    # Debug mode is turned off for production use.
+    app.run('0.0.0.0', port=8080, debug=False)
+    
+     
+
+          
+
+
