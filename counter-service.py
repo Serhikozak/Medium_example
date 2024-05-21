4ceb89ef831e16c6040c1c5afcbccfc458bf8172
Serhikozak <skozakov924@gmail.com>
2024-05-21 13:03:45 +0300

    first def

--- counter-service.py
+++ counter-service.py
@@ -1,2 +1,20 @@
 from flask import Flask, request, jsonfy
 import os
+
+app = Flask(__name__)
+# Define the path for the counter file to store the data in Docker Volume
+COUNTER_FILE = "/data/counter.txt"
+
+def read_counter():
+    """
+    Reads and returns the current counter value from the file.
+    If the file doesn't exist, it return 0.
+    
+    Returns:
+        int: The current counter value.
+    """
+    if os.path.exists(COUNTER_FILE):
+        with open(COUNTER_FILE, "r") as f:
+            return int(f.read().strip())
+    else:
+        return 0    
